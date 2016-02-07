fe84eddc8d44b31fdbdd6b8fb809d0dab9a20ea3
susanr <susanr@MacBook-Pro.local>
2016-02-07 15:37:18 -0700

    Initial Commit

---
+++ Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4
+//
+//  Created by susanr on 2/7/16.
+//  Copyright (c) 2016 susanr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
