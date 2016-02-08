b60a2f6fbf8f89bc019faad0ec74995406d90846
susanr <susanr@MacBook-Pro.local>
2016-02-07 20:09:06 -0700

    commit from Xcode

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  Assignment 4
-//
-//  Created by susanr on 2/7/16.
-//  Copyright (c) 2016 susanr. All rights reserved.
-//
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
+using namespace std;
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
